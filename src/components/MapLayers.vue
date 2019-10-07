74495a27f2c1ee145f82fd874012f8c24afe6cba
Wernimont <mwernimont@igsarmewltmrwm.gs.doi.net>
2019-10-07 11:19:14 -0500

    Map Layers Toggle Button functionality

---
+++ src/components/MapLayers.vue
@@ -0,0 +1,67 @@
+<template></template>
+<script>
+import { MglMap, MglPopup } from "vue-mapbox";
+import { icon } from "@fortawesome/fontawesome-svg-core";
+import mapStyles from "../assets/mapStyles/mapStyles";
+export default {
+  name: "MapLayers",
+  inject: ["mapbox", "map", "actions"],
+  components: {
+    MglPopup
+  },
+  data() {
+    return {
+      control: null
+    };
+  },
+  mounted() {
+    this.createCustomControl();
+  },
+  methods: {
+    createCustomControl() {
+      class customControl {
+        onAdd(map) {
+          let control = this.control;
+          this.map = map;
+          this.control = document.createElement("div");
+          this.control.className = "mapboxgl-ctrl mapboxgl-ctrl-group";
+          this.button = document.createElement("button");
+          this.button.id = "layersIcon";
+          this.button.className = "mapboxgl-ctrl-icon maplayersIcon";
+          this.button.onclick = function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            let toggleDiv = document.getElementById("mapLayersToggleContainer");
+            if (toggleDiv.style.display === "none") {
+              toggleDiv.style.display = "block";
+            } else {
+              toggleDiv.style.display = "none";
+            }
+          };
+          this.icon = document.createElement("span");
+          this.icon.innerHTML = icon({
+            prefix: "fas",
+            iconName: "layer-group"
+          }).html;
+          this.button.appendChild(this.icon);
+          this.control.appendChild(this.button);
+          return this.control;
+        }
+        onRemove() {
+          this.control.parentNode.removeChild(this.control);
+          this.map = undefined;
+        }
+      }
+      const myCustomControl = new customControl();
+      this.map.addControl(myCustomControl, "top-left");
+    }
+  }
+};
+</script>
+<style lang="scss">
+.mapboxgl-ctrl .maplayersIcon svg {
+  width: 18px;
+  height: 18px;
+  margin-top: 4px;
+}
+</style>
