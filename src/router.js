84ab779cbc7be5f4d129df1d2e764d070f17cac2
Aaron Briggs <abriggs@contractor.usgs.gov>
2019-11-26 08:53:04 -0600

    improved code pattern for routes

---
+++ src/router.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import Router from "vue-router";
+import MapBox from './components/MapBox.vue';
+import About from './components/About.vue';
+import Error404 from "./views/Error404";
+
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'MapBox',
+            component: MapBox
+        },
+        {
+            path: '/about',
+            name: 'About',
+            component: About
+        },
+        {
+            path: "/404",
+            name: "Error404",
+            component: Error404
+        },
+        {
+            path: "*",
+            redirect: { name: "Error404" }
+        }
+    ]
+});
+
+
