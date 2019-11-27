c2e169cec8a6e45bb553766706705e0be43434ca
Aaron Briggs <abriggs@contractor.usgs.gov>
2019-11-27 13:06:35 -0600

    added lazy loads to secondary routes

--- src/router.js
+++ src/router.js
@@ -4,8 +4,10 @@ import MapBox from './components/MapBox.vue';
 import About from './views/About.vue';
 import Error404 from "./views/Error404";
 
-
 Vue.use(Router);
+function lazyLoad(view){
+    return() => System.import(`@/views/${view}.vue`)
+}
 
 export default new Router({
     routes: [
@@ -17,12 +19,12 @@ export default new Router({
         {
             path: '/about',
             name: 'About',
-            component: About
+            component: lazyLoad('About')
         },
         {
             path: "/404",
             name: "Error404",
-            component: Error404
+            component: lazyLoad('Error404')
         },
         {
             path: "*",
