a25a42e31e42c4abdd34af6e262bccc87b2aad25
Aaron Briggs <abriggs@contractor.usgs.gov>
2019-12-04 16:22:34 -0600

    basic json format improved

--- src/assets/questionsAndAnswers/questionsAndAnswers.js
+++ src/assets/questionsAndAnswers/questionsAndAnswers.js
@@ -1,21 +1,59 @@
 export default {
     pageContents: {
         title: "Got Questions? We're Here to Provide Answers. ",
-        introText: "So you have a burning question about the National Integrated Water Availability Assessments (IWAAs) application, but you don't see it listed here? No problem!",
+        introText: "So you have a question about the National Integrated Water Availability Assessments (IWAAs) application, but you don't see it listed here? No problem!",
         subtitle: "Your Questions, Our Answers",
-        accordionSections: {
-            SectionOne: {
-                title: "About the Map",
-                questionsAndAnswers: [
-
+        accordionSections: [
+            {
+                "sectionTitle": "About the Map",
+                "questionsAndAnswers": [
+                    {
+                        "question": "What does this map show me?",
+                        "answer": "The " + process.env.APP_VUE_TITLE + "shows the latest available daily estimates of natural water storage for approximately 110,000 regions across the lower forty-eight states. Map shading indicates the current natural water storage relative to historical conditions for this time of year."
+                    },
+                    {
+                       "question": "",
+                       "answer": ""
+                    },
+                    {
+                        "question": "",
+                        "answer": ""
+                    },
+                    {
+                        "question": "",
+                        "answer": ""
+                    },
+                    {
+                        "question": "",
+                        "answer": ""
+                    }
                 ]
             },
-            SectionTwo: {
-                title: "Deep in the Science",
-                questionsAndAnswers: [
-
+            {
+                "sectionTitle": "Deep in the Science",
+                "questionsAndAnswers": [
+                    {
+                        "question": "",
+                        "answer": ""
+                    },
+                    {
+                        "question": "",
+                        "answer": ""
+                    },
+                    {
+                        "question": "",
+                        "answer": ""
+                    },
+                    {
+                        "question": "",
+                        "answer": ""
+                    },
+                    {
+                        "question": "",
+                        "answer": ""
+                    }
                 ]
             }
-        }
+        ]
     }
 }
