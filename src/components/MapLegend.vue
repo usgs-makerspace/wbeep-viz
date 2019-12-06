571c47194ed291a955879254fdf44c9ce834c66f
Wernimont <mwernimont@igsarmewltmrwm.gs.doi.net>
2019-12-06 09:43:00 -0600

    Hover for legend tabs and larger font ofr SVG

--- src/components/MapLegend.vue
+++ src/components/MapLegend.vue
@@ -289,4 +289,13 @@ $buttonActiveTextColor: #fff;
     font-size: 0.9em;
   }
 }
+
+@media screen and (min-width: 960px){
+  .tab {
+    &:hover{
+      background: $buttonColorActive;
+      color: $buttonActiveTextColor;
+    }
+  }
+}
 </style>
