3b5077f4d1f7f9020902531858cd02724f9e8255
Marty Wernimont <mwernimont87@gmail.com>
2020-09-01 13:02:28 -0500

    lint fixes

--- src/views/waterUse/WaterUse.vue
+++ src/views/waterUse/WaterUse.vue
@@ -1,14 +1,39 @@
 <template>
-  <div id="water-use-container" class="centeredContent waterUseFlex">
-    <div id="water-use-content" class="waterUseFlex">
+  <div
+    id="water-use-container"
+    class="centeredContent waterUseFlex"
+  >
+    <div
+      id="water-use-content"
+      class="waterUseFlex"
+    >
       <LoadingScreenInternal />
-      <div id="buttonsContainer" class="centeredContent">
-        <button class="waterUseButton">Thermoelectric</button>
-        <button class="waterUseButton">Irrigation</button>
-        <button class="waterUseButton">Public Supply</button>
+      <div
+        id="buttonsContainer"
+        class="centeredContent"
+      >
+        <button class="waterUseButton">
+          Thermoelectric
+        </button>
+        <button class="waterUseButton">
+          Irrigation
+        </button>
+        <button class="waterUseButton">
+          Public Supply
+        </button>
       </div>
-      <div id="waterUseMapContainer" class="bordered">Map</div>
-      <div id="waterUseBarChartContainer" class="bordered">BarChart</div>
+      <div
+        id="waterUseMapContainer"
+        class="bordered"
+      >
+        Map
+      </div>
+      <div
+        id="waterUseBarChartContainer"
+        class="bordered"
+      >
+        BarChart
+      </div>
     </div>
   </div>
 </template>
