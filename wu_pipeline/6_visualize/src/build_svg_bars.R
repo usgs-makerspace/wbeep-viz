8aea8d2802959a21120b67cb196f48c73e823b49
Platt <lplatt@usgs.gov>
2020-09-10 12:30:19 -0500

    scientific notation for y axis label

--- wu_pipeline/6_visualize/src/build_svg_bars.R
+++ wu_pipeline/6_visualize/src/build_svg_bars.R
@@ -75,7 +75,7 @@ add_y_axis <- function(svg, wu_dat, svg_height) {
     xml_add_sibling("text", id = "yAxisLabelLow", class = "wu-bars-axis", `text-anchor`="end",
                     x = -20, y = svg_height - 2, "0") %>% 
     xml_add_sibling("text", id = "yAxisLabelHigh", class = "wu-bars-axis", `text-anchor`="end",
-                    x = -20, y = 10, sprintf("%s", signif(max_wu_val,5))) %>% 
+                    x = -20, y = 10, formatC(max_wu_val, format = "e", digits = 3)) %>% 
     xml_add_sibling("text", id = "yAxisTitle", class = "wu-bars-axis", "Daily water use, mgd", `text-anchor`="middle",
                     transform=sprintf("rotate(-90) translate(-%s -20)", svg_height/2))
   
