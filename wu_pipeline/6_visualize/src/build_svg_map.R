c8b1dccba4d0103bbcd60bb9207f8575a0aa92c6
Platt <lplatt@usgs.gov>
2020-10-08 11:54:53 -0500

    add a few extra classes

--- wu_pipeline/6_visualize/src/build_svg_map.R
+++ wu_pipeline/6_visualize/src/build_svg_map.R
@@ -87,13 +87,13 @@ add_legend <- function(svg, legend_size_dat, svg_width, svg_height, wu_type_cd) 
   # Add miniscule dot to show that those values represent non-zero water use data
   y_pos_tiny_dot <- 10
   wu_legend %>% 
-    xml_add_child('circle', cx = 0, cy = y_pos_tiny_dot, r = 1) %>% 
+    xml_add_child('circle', cx = 0, cy = y_pos_tiny_dot, r = 1, class = "smallDot") %>% 
     xml_add_sibling('text', x = label_line_length*1.10, y = y_pos_tiny_dot, "< 1")
   
   # Add text above circles
   wu_legend %>% 
     xml_add_child('text', x = label_line_length/2, y = -max(legend_size_dat$radius)*2*1.50, 
-                  "Water use, million gallons per day")
+                  "Water use, million gallons per day", class = "legendTitle")
 }
 
 add_circle_with_label <- function(svg, r, label, label_line_length) {
