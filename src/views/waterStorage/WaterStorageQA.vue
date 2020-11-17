9f24a45910a64a11e22de70124a8567e5304d580
Marty Wernimont <mwernimont87@gmail.com>
2020-11-17 16:05:21 -0600

    Tabs are working

--- src/views/waterStorage/WaterStorageQA.vue
+++ src/views/waterStorage/WaterStorageQA.vue
@@ -1,10 +1,79 @@
 <template>
-  <h1>Storage questions will go here</h1>
+  <div class="usa-prose">
+    <!-- Water Storage -->
+    <h2>Water Storage</h2>
+    <div
+      v-for="section in pageContents.waterStorageAccordionSections"
+      :key="section.sectionTitle"
+    >
+      <h6 class="section-title">
+        {{ section.sectionTitle }}
+        <router-link
+          to="/"
+        >
+          <button>
+            Return to Water Storage
+          </button>
+        </router-link>
+      </h6>
+      <div class="usa-accordion usa-accordion--bordered">
+        <div
+          v-for="question in section.questionsAndAnswers"
+          :key="question.question"
+        >
+          <h2 class="usa-accordion__heading">
+            <button
+              class="usa-accordion__button"
+              aria-expanded="true"
+              :aria-controls="question.id"
+            >
+              {{ question.question }}
+            </button>
+          </h2>
+          <div
+            :id="question.id"
+            class="usa-accordion__content usa-prose"
+          >
+            <p><span v-html="question.answer" /></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      id="referencesContainer"
+      class="usa-prose"
+      >
+      <h2>{{ titleWaterStorage }}</h2>
+      <div
+        v-for="reference in referencesWaterStorage"
+        :key="reference.reference"
+      >
+        <p class="about-ref">
+          {{ reference.reference }}
+          <a
+            :href="reference.href"
+            target="_blank"
+          >
+            <span v-html="reference.href" />
+          </a>
+        </p>
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
+    import questionsAndAnswers from "@/assets/questionsAndAnswers/questionsAndAnswers";
+    import references from "@/assets/references/references";
     export default {
-        name: 'WaterStorageQA'
+        name: 'WaterStorageQA',
+        data() {
+          return {
+              pageContents: questionsAndAnswers.pageContents,
+              titleWaterStorage: references.referenceSectionWaterStorage.title,
+              referencesWaterStorage: references.referenceSectionWaterStorage.references
+          };
+        }
     }
 </script>
 
