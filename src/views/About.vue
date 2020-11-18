5b55f937ee4e06134e00c64aee7c34e2af7e1da3
Marty Wernimont <mwernimont87@gmail.com>
2020-11-18 07:49:59 -0600

    About page content

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,66 @@
   <div id="aboutContainer">
     <WorkInProgressWarning />
     <div id="aboutContent">
-      About Page
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel faucibus quam. Fusce nulla mauris, aliquam ut ullamcorper vel, mollis et ex. Sed eu sapien felis. Aenean erat libero, accumsan ut lorem consequat, mattis fermentum purus. Sed mollis, dolor in faucibus venenatis, dui enim fringilla sem, a pretium tortor odio id ipsum. Aliquam rhoncus lorem dolor. Nulla imperdiet pretium erat in euismod. Donec porttitor rutrum nibh quis dapibus. Sed sit amet suscipit nisi, eu pellentesque magna. Cras sed lorem eu elit imperdiet sodales. Sed nec pulvinar diam. Quisque vulputate tincidunt sem eu eleifend. Nullam nec est ante. Etiam id tortor eleifend, molestie nunc eu, dignissim lectus. Sed vehicula in leo a cursus. Proin fringilla metus ut nisl porttitor ornare.</p>
+      <h2><strong>Water Availability Mapper:</strong> Water Storage, Water Temperature, Water Use</h2>
+      <div class="application" id="waterStorage">
+        <h3>Water Storage</h3>
+        <img class="applicationImage" src="@/assets/images/about/storage.jpg"/>
+        <p>
+          Last year, on December 18, 2019, a research team that includes hydrologic modelers, software developers, 
+          cloud specialists, and data visualization experts released the first application in a set of applications 
+          that represent a national view of <a href="https://labs.waterdata.usgs.gov/estimated-availability/index.html#/" target="_blank">water availability</a>. While only a concept prototype, for the first time 
+          the USGS began delivering a daily water storage product that integrates our active monitoring with continuous 
+          modeling. The water storage application uses modeling to enhance our active monitoring network and provides 
+          comprehensive reporting at more than 100,000 watershed units. The National <a href="https://labs-beta.waterdata.usgs.gov/estimated-availability/index.html#/water-storage" target="_blank">water storage application</a> 
+          compares current natural water storage to daily historical values. 
+        </p>
+        <p>
+          To expand upon the current visualization of daily estimated water storage, the second and third deliverables 
+          of the USGS IWAAs program are map-based displays of modeled daily stream temperatures, and modeled 2015 water 
+          use estimates. Together, these three indicators provide water quality and availability summaries in response to 
+          the <a href="https://www.whitehouse.gov/presidential-actions/presidential-memorandum-promoting-reliable-supply-delivery-water-west/" target="_blank">Presidential Memo on Water in the West</a>.
+        </p>
+      </div>
+      <div class="application" id="waterTemp">
+        <h3>Water Temperature</h3>
+        <img class="applicationImage" src="@/assets/images/about/temp.jpg"/>
+        <p>
+          A new addition to the Water Availability mapper is a 
+          <a href="https://labs-beta.waterdata.usgs.gov/estimated-availability/index.html#/water-temperature" target="_blank">
+            National stream temperature application
+          </a>.  
+          This application displays a near real-time indicator of daily-average stream temperature for approximately 
+          58,000 stream segments. These modeled results provide comprehensive spatial improvements and enhance the active 
+          monitoring network of XX continuous temperature monitors currently operated by USGS. The daily model run requires 
+          a summary of the weather (precipitation and temperature) for a complete day. A sequence of operations begin at midnight 
+          starting with collecting observed weather data, using 
+          <a href="https://pubs.er.usgs.gov/publication/tm6B7" target="_blank">
+            Precipitation-Runoff Modeling System (PRMS)
+          </a> 
+          and SNTemp stream temperature model to calculate estimates of natural water temperature, then sending those data from the Cloud 
+          to the mapping application to update symbology for all stream segments.  This prototype application will be enhanced with new 
+          modeling technologies in future releases that will increase predication accuracy and may increase the spatial representation 
+          of data. 
+        </p>
+      </div>
+      <div class="application" id="waterUse">
+        <h3>Water Use</h3>
+        <img class="applicationImage" src="@/assets/images/about/use.png"/>
+        <p>
+          The third application in the water availability application is the 
+          <a href="https://labs-beta.waterdata.usgs.gov/estimated-availability/index.html#/water-use" target="_blank">
+            National water use application
+          </a>.  
+          Water Use data for each county and state were compiled from various sources (depending on the category 
+          of use and the information available for each state) into annual total withdrawals representing 2015. 
+          These annual data were downscaled both spatially and temporally to represent average daily values for 
+          each watershed (12-digit hydrologic unit code) using methods that include reference to climate, 
+          population and other parameters. Withdrawals are reported for nine categories of use, but these maps 
+          show withdrawals for only thermoelectric power, irrigation, and public supply which make up more than 90% 
+          of the total withdrawals in the U.S. (<a href="https://pubs.er.usgs.gov/publication/cir1441" target="_blank">Dieter and others, 2018</a>).  
+          The accurate estimation of water use nationally at the HUC 12 watershed scale provides a consistent national source of water use data for the first time. 
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -26,5 +84,46 @@
     padding: 10px;
     max-width: 960px;
     margin:0 auto;
+    h2{
+      text-align: center;;
+      font-weight: normal;
+      margin-bottom: 20px;
+      padding-bottom: 10px;
+      border-bottom: 1px solid #003366;
+    }
+    h3{
+      font-size: 1.3em;
+    }
+  }
+  a{
+    text-decoration: none;
+    color: #003366;
+    font-weight: bold;
+    outline: none;
+    &:hover{
+      text-decoration: underline;
+    }
+  }
+  .application{
+    min-height: 100px;
+    margin-bottom: 20px;
+
+    p{
+      margin-top: 10px;
+      line-height: 1.5em;
+    }
+  }
+  @media screen and (min-width: 600px){
+    .application{
+      margin-bottom: 40px;
+      img{
+        width: 40%;
+        float: left;
+        margin-right: 10px;
+      }
+      p{
+        font-size: 1.1em;
+      }
+    }
   }
 </style>
