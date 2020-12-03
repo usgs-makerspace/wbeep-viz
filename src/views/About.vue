0021797dbd92bce3599eb3c7a8da54c4005b7a9d
Marty Wernimont <mwernimont87@gmail.com>
2020-12-03 12:01:13 -0600

    Additional about page text

--- src/views/About.vue
+++ src/views/About.vue
@@ -62,6 +62,51 @@
           The accurate estimation of water use nationally at the HUC 12 watershed scale provides a consistent national source of water use data for the first time. 
         </p>
       </div>
+      <div class="application" id="operationalPipeline">
+        <h3>Operational Pipeline</h3>
+        <p>
+          In order to update the water storage and water temperature applications for near-real time conditions, 
+          a series of steps are completed daily. The major steps in the operational pipeline include: 1) obtaining 
+          and formatting gridded climate inputs, 2) hydrologic model execution, 3) post-processing of model outputs, 
+          and 4) updating the water storage and water temperature visualizations. Climate inputs of daily maximum and 
+          minimum air temperature and precipitation accumulation from the GridMET dataset (<a href="https://doi.org/10.1002/joc.3413" target="_blank">Abatzoglou, 2013</a>) 
+          are currently used in the hydrologic model simulations that are used for the water storage and stream temperature applications. 
+          A containerized workflow has been implemented for this process to allow for modularization and flexibility of various 
+          components, as well as allowing for efficient troubleshooting. 
+        </p>
+        <div id="diagram">Diagram</div>
+        <p>
+          Each container in the workflow, as shown in the accompanying diagram, executes a specific, independent, task. 
+          The first container checks to see if the climate inputs have been updated for the latest complete day. The second 
+          container downloads the climate inputs and processes the data from their native grid to the model spatial units using 
+          a contributing area-weighted averaging algorithm. The third container executes the hydrologic model using with the initial 
+          conditions (restart file) from the previous model run and the updated climate data. The fourth container updates the initial 
+          conditions files for the next time that the model is executed. The fifth through nineth containers access the converted model 
+          outputs and update the visualizations of water storage and water temperature.
+        </p>
+        <p>
+          To complete step 4 of the operational pipeline and produce the daily-updating vector tileset to visualize modeled water storage 
+          or temperature, a series actions are completed in containers six through nine the Amazon cloud.  The sixth container receives 
+          the geospatial data and converts them into GeoJSON. In the seventh container the GeoJSON data are retrieved and converted into 
+          vector tiles which are stored in the Cloud. The seventh container of the pipeline combines (joins) the vector tile data with the 
+          latest model simulation results and places these data-infused tiles back into the Cloud. This part of the pipeline used on a daily 
+          basis to update the vector tile data with the latest available model simulation results. Finally, the vector tiles are visualized 
+          within the web application.  
+        </p>
+      </div>
+      <div class="application" id="operationalPipeline">
+        <h3>Disclaimer</h3>
+        <p>
+          <em>This software has been approved for release by the U.S. Geological Survey (USGS). Although the software has been subjected to 
+          rigorous review, the USGS reserves the right to update the software as needed pursuant to further analysis and review. No warranty, 
+          expressed or implied, is made by the USGS or the U.S. Government as to the functionality of the software and related material nor 
+          shall the fact of release constitute any such warranty. Furthermore, the software is released on condition that neither the USGS nor 
+          the U.S. Government shall be held liable for any damages resulting from its authorized or unauthorized use.</em>
+        </p>
+        <p>
+          <em>Any use of trade, firm, or product names is for descriptive purposes only and does not imply endorsement by the U.S. Government.</em>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -109,8 +154,14 @@
     margin-bottom: 20px;
 
     p{
-      margin-top: 10px;
       line-height: 1.5em;
+    }
+    #diagram{
+      background: rgba(0,0,0,.5);
+      padding: 20px 0;
+      font-size: 2em;
+      text-align: center;
+      color: #fff;
     }
   }
   @media screen and (min-width: 600px){
